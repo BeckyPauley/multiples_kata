ee25f0691fcda8e8830e3fb83232260830442a69
Becky P <64131207+BeckyPauley@users.noreply.github.com>
2020-07-29 13:06:03 +0100

    Kata solution to find multiples of 3 and 5

---
+++ multiples.py
@@ -0,0 +1,16 @@
+def solution(number):
+    numbers = []
+    for i in range (1, number):
+        if i % 3 == 0:
+            numbers.append(i)
+        if i % 5 == 0 and i not in numbers:
+            numbers.append(i)
+    print(numbers)
+    total = 0
+    for n in numbers:
+        total += n
+        print(f"adding {n}")
+    print(total)
+    return total
+
+solution(15)
